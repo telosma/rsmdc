25d75fc97166c8cc43cf0b54e24a14a49a121447
rsmdc <koharukoharuko0426@gmail.com>
2019-04-11 15:30:43 +0900

    change css property in custom elements style

--- button/src/rs-button.vue
+++ button/src/rs-button.vue
@@ -40,7 +40,7 @@ export default {
 
 .rs-button {
   // base
-  padding: var(--rs-button--padding, 0 #{$rs-button-horizontal-padding} 0 #{$rs-button-horizontal-padding});
+  padding: var(--rs-button--padding, 0 $rs-button-horizontal-padding 0 $rs-button-horizontal-padding);
   border-radius: var(--rs-button--border-radius, 4px);
   font-size: var(--rs-button--font-size, 0.875rem);
   box-shadow: var(--rs-button--box-shadow, none);
@@ -65,24 +65,24 @@ export default {
 
   &:not(:disabled) {
     background-color: var(--rs-button_not_disabled--background-color, transparent);
-    color: var(--rs-button_not_disabled--color, #{$rs-theme-primary});
+    color: var(--rs-button_not_disabled--color, $rs-theme-primary);
     border-color: var(--rs-button_not_disabled--border-color, none);
   }
 
   &:disabled {
     box-shadow: var(--rs-button_disabled--box-shadow);
     background-color: var(--rs-button_disabled--background-color, transparent);
-    color: var(--rs-button_disabled--color, #{$rs-button-disabled-ink-color});
+    color: var(--rs-button_disabled--color, $rs-button-disabled-ink-color);
     border-color: var(--rs-button_disabled--border-color);
   }
 
   &::before{
-    background-color: var(--rs-ripple_before--background-color, #{$rs-theme-primary});
+    background-color: var(--rs-ripple_before--background-color, $rs-theme-primary);
     content: var(--rs-ripple_before--content, '');
   }
 
   &::after {
-    background-color: var(--rs-ripple_after--background-color, #{$rs-theme-primary});
+    background-color: var(--rs-ripple_after--background-color, $rs-theme-primary);
     content: var(--rs-ripple_after--content, '');
   }
 
@@ -92,29 +92,28 @@ export default {
 
 
   &:not(.rs-ripple-upgraded):focus::before { // @mixin rs-states-focus-opacityのfalse
-    @include set-rs-feat-animation(transition-duration, var(--rs-ripple_not-upgraded_focus_before--transition-duration, 75ms));
-    @include set-rs-feat-structure(opacity, var(--rs-ripple_not-upgraded_focus_before--opacity, rs-states-opacity(primary, focus)));
+    transition-duration: var(--rs-ripple_not-upgraded_focus_before--transition-duration, 75ms);
+    opacity: var(--rs-ripple_not-upgraded_focus_before--opacity, rs-states-opacity(primary, focus));
   }
 
   &.rs-ripple-upgraded--background-focused::before {
-    @include set-rs-feat-animation(transition-duration, var(--rs-upgraded_-background-focused_before--transition-duration, 75ms));
-    @include set-rs-feat-structure(opacity, var(--rs-upgraded_-background-focused_before--opacity, rs-states-opacity(primary, focus)));
+    transition-duration: var(--rs-upgraded_-background-focused_before--transition-duration, 75ms);
+    opacity: var(--rs-upgraded_-background-focused_before--opacity, rs-states-opacity(primary, focus));
   }
 
   &:not(.rs-ripple-upgraded) {
     &::after {
-      @include set-rs-feat-animation(transition, var(--rs-ripple_not-upgraded_after--transition, opacity #{$rs-ripple-fade-out-duration} linear));
+      transition: var(--rs-ripple_not-upgraded_after--transition, opacity $rs-ripple-fade-out-duration linear);
     }
 
     &:active::after {
-      @include set-rs-feat-animation(transition-duration, var(--rs-ripple_not-upgraded_active_after--transition-duration, #{$rs-ripple-fade-in-duration}));
-      @include set-rs-feat-structure(opacity, var(--rs-ripple_not-upgraded_active_after--opacity, rs-states-opacity(primary, press)));
+      transition-duration: var(--rs-ripple_not-upgraded_active_after--transition-duration, $rs-ripple-fade-in-duration);
+      opacity: var(--rs-ripple_not-upgraded_active_after--opacity, rs-states-opacity(primary, press));
     }
   }
 
-
   &.rs-ripple-upgraded {
-    @include set-rs-feat-color(--rs-ripple-fg-opacity, var(--rs-ripple-upgraded--rs-ripple-fg-opacity, rs-states-opacity(primary, press)));
+    --rs-ripple-fg-opacity: var(--rs-ripple-upgraded--rs-ripple-fg-opacity, #{rs-states-opacity(primary, press)});
   }
 
 }
