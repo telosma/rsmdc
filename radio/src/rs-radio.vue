21f7cd3bb055bcc94193abf488b5ba508e77c7cb
rsmdc <koharukoharuko0426@gmail.com>
2019-04-11 17:15:33 +0900

    add rs-radio.vue

---
+++ radio/src/rs-radio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="rs-form-field">
+    <div class="rs-radio">
+      <input class="rs-radio__native-control" type="radio" id="radio-1" name="radios" checked>
+      <div class="rs-radio__background">
+        <div class="rs-radio__outer-circle"></div>
+        <div class="rs-radio__inner-circle"></div>
+      </div>
+    </div>
+    <label for="radio-1">Radio 1</label>
+  </div>
+</template>
+<script>
+
+export default {
+  
+}
+</script>
+<style lang="scss">
+@import "../assets/rs-radio";
+@import "../../form-field/assets/rs-form-field"
+
+
+
+</style>
+
+
