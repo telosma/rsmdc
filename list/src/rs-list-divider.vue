19dc4e46b15423ab7d9b972ba995bd3be342653e
rsmdc <koharukoharuko0426@gmail.com>
2019-04-17 11:09:27 +0900

    add list-divider

--- list/src/rs-list-divider.vue
+++ list/src/rs-list-divider.vue
@@ -1,4 +1,21 @@
 <template>
   <hr class="rs-list-divider"></hr>
 </template>
+<style lang="scss">
+@import '../rs-list';
+
+.rs-list-divider {
+  $divider-color: if(
+    rs-theme-tone($rs-theme-background) == "dark",
+    $rs-list-divider-color-on-dark-bg,
+    $rs-list-divider-color-on-light-bg
+  );
+  width: var(--rs-list-divider--width);
+  margin: var(--rs-list-divider--margin, 0);
+  margin-left: var(--rs-list-divider--margin-left, 0);
+  margin-right: var(--rs-list-divider--margin-right, 0);
+  border-bottom-color: var(--rs-list-divider--border-bottom-color, $divider-color);
+}
+</style>
+
 
