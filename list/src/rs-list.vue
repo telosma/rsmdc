1a131d7a17cb8fcce0e0931e7a414b3156de84c9
rsmdc <koharukoharuko0426@gmail.com>
2019-05-07 15:36:50 +0900

    [WIP] modify list component

--- list/src/rs-list.vue
+++ list/src/rs-list.vue
@@ -21,8 +21,11 @@ export default {
 
 <style lang="scss">
 @import '../rs-list';
+@import '../mixins';
+@import "../../typography/mixins";
 
 .rs-list {
+  @include rs-list-base_;
   padding-top: var(--rs-list--padding-top, 8px);
   padding-bottom: var(--rs-list--padding-bottom, 8px);
   font-size: var(--rs-list--font-size);
