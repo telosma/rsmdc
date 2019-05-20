4e2d4a222065adda5d2b151a4813a937a792a234
rsmdc <koharukoharuko0426@gmail.com>
2019-05-20 13:24:03 +0900

    remove unnecessary code

--- packages/top-app-bar/src/rs-top-app-bar-actions.vue
+++ packages/top-app-bar/src/rs-top-app-bar-actions.vue
@@ -4,23 +4,9 @@
   </div>
 </template>
 <script>
-import { RSRipple } from '@rsmdc/ripple'
-
 export default {
   mounted() {
-    this.$nextTick()
-    .then(this.fixSlot.bind(this))
-    .then(() => {
-      const slotChildren = this.$refs.slotContainer.querySelector('slot').assignedNodes()
-      const elements = Array.from(slotChildren).filter(child => child.nodeType === 1)
-      elements.forEach(el => {
-        if(el.nodeName === 'I') {
-          const ripple = new RSRipple(el)
-          ripple.unbounded = true
-          el.setAttribute('tabindex', 0)
-        }
-      })
-    })
+    this.$nextTick().then(this.fixSlot.bind(this))
   },
   methods: {
     fixSlot() {
@@ -32,9 +18,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../mixins';
-@import '../variables';
-@import '@rsmdc/ripple/rs-mixins';
 
 :host {
   --_rs-top-app-bar-actions: true;
@@ -48,58 +31,5 @@ export default {
   justify-content: center;
   padding-right: var(--rs-top-app-bar-actionItem--padding-right);
 }
-
-
-::slotted(i) {
-  @include rs-top-app-bar-icon_;
-  transition: var(--rs-top-app-bar-actionItem--transition);
-  color: var(--rs-top-app-bar-actionItem--color, inherit);
-
-  background-image: var(--rs-top-app-bar-actionItem--background-image);
-  background-repeat: no-repeat;
-  background-size: 24px;
-  background-position: center;
-}
-
-::slotted(i)::before {
-  background-color: var(--rs-ripple_before--background-color, $rs-theme-on-primary);
-  content: var(--rs-ripple_before--content, '');
-}
-
-::slotted(i)::after {
-  background-color: var(--rs-ripple_after--background-color, $rs-theme-on-primary);
-  content: var(--rs-ripple_after--content, '');
-  transition: opacity 150ms linear;
-}
-
-::slotted(i:hover)::before {
-  opacity: var(--rs-ripple_hover_before--opacity, rs-states-opacity(on-primary, hover));
-}
-
-::slotted(i:not(.rs-ripple-upgraded):focus)::before {
-  transition-duration: var(--rs-ripple_not-upgraded_focus_before--transition-duration, 75ms);
-  opacity: var(--rs-ripple_not-upgraded_focus_before--opacity, rs-states-opacity(on-primary, focus));
-}
-
-::slotted(.rs-ripple-upgraded--background-focused)::before {
-  transition-duration: var(--rs-upgraded_-background-focused_before--transition-duration, 75ms);
-  opacity: var(--rs-upgraded_-background-focused_before--opacity, rs-states-opacity(on-primary, focus));
-}
-
-::slotted(i:not(.rs-ripple-upgraded))::after {
-  transition: var(--rs-ripple_not-upgraded_after--transition, opacity $rs-ripple-fade-out-duration linear);
-}
-
-::slotted(i:not(.rs-ripple-upgraded):active)::after {
-  transition-duration: var(--rs-ripple_not-upgraded_active_after--transition-duration, $rs-ripple-fade-in-duration);
-  opacity: var(--rs-ripple_not-upgraded_active_after--opacity, rs-states-opacity(on-primary, press));
-}
-
-::slotted(.rs-ripple-upgraded) {
-  --rs-ripple-fg-opacity: var(--rs-ripple-upgraded--rs-ripple-fg-opacity, #{rs-states-opacity(on-primary, press)});
-}
-
-@include rs-ripple-surface-slotted;
-@include rs-ripple-radius-unbounded-slotted;
 
 </style>
--- packages/top-app-bar/src/rs-top-app-bar-nav.vue
+++ packages/top-app-bar/src/rs-top-app-bar-nav.vue
@@ -11,17 +11,8 @@ export default {
     this.$nextTick()
     .then(this.fixSlot.bind(this))
     .then(() => {
-      const slotChildren = this.$refs.slotContainer.querySelector('slot').assignedNodes()
-      if(slotChildren.length > 0) {
-        const elements = Array.from(slotChildren).filter(child => child.nodeType === 1)
-        elements.forEach(child => {
-          const ripple = new RSRipple(child)
-          ripple.unbounded = true
-        })
-      } else {
-        const ripple = new RSRipple(this.$el)
-        ripple.unbounded = true
-      }
+      const ripple = new RSRipple(this.$el)
+      ripple.unbounded = true
     })
   },
   methods: {
@@ -92,57 +83,4 @@ export default {
     --rs-ripple-fg-opacity: var(--rs-ripple-upgraded--rs-ripple-fg-opacity, #{rs-states-opacity(on-primary, press)});
   }
 }
-
-/* using slot */
-::slotted(i) {
-  @include rs-top-app-bar-icon_;
-  color: var(--rs-top-app-bar-navigationIcon--color, inherit);
-
-  background-image: var(--rs-top-app-bar-navigationIcon--background-image);
-  background-repeat: no-repeat;
-  background-size: 24px;
-  background-position: center;
-}
-
-::slotted(i)::before {
-  background-color: var(--rs-ripple_before--background-color, $rs-theme-on-primary);
-  content: var(--rs-ripple_before--content, '');
-}
-
-::slotted(i)::after {
-  background-color: var(--rs-ripple_after--background-color, $rs-theme-on-primary);
-  content: var(--rs-ripple_after--content, '');
-  transition: opacity 150ms linear;
-}
-
-::slotted(i:hover)::before {
-  opacity: var(--rs-ripple_hover_before--opacity, rs-states-opacity(on-primary, hover));
-}
-
-::slotted(i:not(.rs-ripple-upgraded):focus)::before {
-  transition-duration: var(--rs-ripple_not-upgraded_focus_before--transition-duration, 75ms);
-  opacity: var(--rs-ripple_not-upgraded_focus_before--opacity, rs-states-opacity(on-primary, focus));
-}
-
-::slotted(.rs-ripple-upgraded--background-focused)::before {
-  transition-duration: var(--rs-upgraded_-background-focused_before--transition-duration, 75ms);
-  opacity: var(--rs-upgraded_-background-focused_before--opacity, rs-states-opacity(on-primary, focus));
-}
-
-::slotted(i:not(.rs-ripple-upgraded))::after {
-  transition: var(--rs-ripple_not-upgraded_after--transition, opacity $rs-ripple-fade-out-duration linear);
-}
-
-::slotted(i:not(.rs-ripple-upgraded):active)::after {
-  transition-duration: var(--rs-ripple_not-upgraded_active_after--transition-duration, $rs-ripple-fade-in-duration);
-  opacity: var(--rs-ripple_not-upgraded_active_after--opacity, rs-states-opacity(on-primary, press));
-}
-
-::slotted(.rs-ripple-upgraded) {
-  --rs-ripple-fg-opacity: var(--rs-ripple-upgraded--rs-ripple-fg-opacity, #{rs-states-opacity(on-primary, press)});
-}
-
-@include rs-ripple-surface-slotted;
-@include rs-ripple-radius-unbounded-slotted;
-
 </style>
