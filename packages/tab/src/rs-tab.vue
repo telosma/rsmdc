1ca4bcebb67de9107f718a18cf7db0ea46b1cc99
rsmdc <koharukoharuko0426@gmail.com>
2019-05-27 10:22:21 +0900

    [WIP] modify tab to custom elements

---
+++ packages/tab/src/rs-tab.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rs-tab-bar" role="tablist">
+    <div class="rs-tab-scroller">
+      <div class="rs-tab-scroller__scroll-area">
+        <div class="rs-tab-scroller__scroll-content" ref="slotContainer">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
