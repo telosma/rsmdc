8214678ad24ba7090524714e4651970ce2bf369c
smspp <smspring0426@gmail.com>
2019-05-28 14:59:39 +0900

    set indicator to select tab

--- packages/tabs/src/rs-tab-bar.vue
+++ packages/tabs/src/rs-tab-bar.vue
@@ -11,14 +11,84 @@
 </template>
 <script>
 export default {
+  data() {
+    return {
+      el: '',
+      host: '',
+      scrollLeft: 0
+    }
+  },
+  watch: {
+    el() {
+      this.host = this.el.parentNode.host
+    },
+    host() {
+      this.leftPosition = this.host.getBoundingClientRect().left
+    }
+  },
   mounted() {
-    this.$nextTick().then(this.fixSlot.bind(this))
+    this.$nextTick()
+    .then(this.fixSlot.bind(this))
+    .then(() => {
+      const slotChildren = Array.from(this.$el.querySelector('slot').assignedNodes()).filter(node => node.nodeType === 1)
+      slotChildren.forEach((child, i) => {
+        child.addEventListener('click', () => {
+          const width = this.getElementProperty(child, 'width')
+          const indicator = this.$el.querySelector('.rs-tab-indicator')
+          const scrollArea = this.$el.querySelector('.rs-tab-scroller__scroll-area')
+          const rect = child.getBoundingClientRect()
+          const indicatorLeft = rect.left - this.leftPosition + scrollArea.scrollLeft
+          indicator.style.setProperty('width', width)
+          indicator.style.setProperty('left', `${indicatorLeft}px`)
+
+          if(i === 0 || i === slotChildren.length-1) { return }
+
+          const scrollAreaWidth = parseInt(this.getElementProperty(scrollArea, 'width').replace('px', ''))
+          const childWidth = parseInt(this.getElementProperty(child, 'width').replace('px', ''))
+          const rightElRect = slotChildren[i+1].getBoundingClientRect()
+          const leftElRect = slotChildren[i-1].getBoundingClientRect()
+
+          // if(scrollAreaWidth < rightElRect.right) {
+          //   console.log(1)
+          //   const width = parseInt(this.getElementProperty(slotChildren[i+1], 'width').replace('px', '')) / 2
+          //   this.scrollLeft += rightElRect.right - scrollAreaWidth
+          //   this.scrollLeft = scrollAreaWidth
+          //   console.log(this.scrollLeft)
+          //   console.log(rightElRect.right - scrollAreaWidth)
+          //   scrollArea.scrollTo({
+          //     top: 0,
+          //     left: rightElRect.right - scrollAreaWidth,
+          //     behavior: 'smooth'
+          //   })
+          // } else if(0 > leftElRect.left) {
+          //   console.log(2)
+          //   const width = parseInt(this.getElementProperty(slotChildren[i-1], 'width').replace('px', '')) / 2
+          //   this.scrollLeft = leftElRect.left + scrollAreaWidth
+          //   console.log(this.scrollLeft)
+          //   console.log(leftElRect.left + scrollAreaWidth)
+          //   scrollArea.scrollTo({
+          //     top: 0,
+          //     left: leftElRect.left + scrollAreaWidth,
+          //     behavior: 'smooth'
+          //   })
+          // }
+        })
+      })
+    })
     this.el = this.$el
   },
   methods: {
     fixSlot() {
       this.$refs.slotContainer.innerHTML = ''
       this.$refs.slotContainer.append(document.createElement('slot'))
+      const div = document.createElement('div')
+      div.classList.add('rs-tab-indicator')
+      this.$refs.slotContainer.append(div)
+    },
+    getElementProperty(el, prop) {
+      const style = window.getComputedStyle(el)
+      const value = String(style.getPropertyValue(prop)).trim()
+      return value
     }
   }
 }
@@ -26,6 +96,8 @@ export default {
 
 
 <style lang="scss">
+@import "@rsmdc/animation/variables";
+@import "@rsmdc/theme/variables";
 
 .rs-tab-bar {
   width: var(--rs-tab-bar--width, 100%);
@@ -38,7 +110,6 @@ export default {
 .rs-tab-scroller__scroll-area {
   -webkit-overflow-scrolling: touch;
   display: flex;
-  overflow-x: hidden;
   overflow-x: scroll;
 
   &::-webkit-scrollbar {
@@ -60,7 +131,27 @@ export default {
   justify-content: var(--rs-tab-scroller--justify-content);
 }
 
+.rs-tab-indicator {
+  height: 2px;
+  pointer-events: none;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  transition: 
+    left 0.2s ease-in-out,
+    width 0.2s ease-in-out;
+  width: var(--rs-tab-indicator--width, 100px);
+  height: var(--rs-tab-indicator--height, 2px);
+  background-color: var(--rs-tab-indicator--background-color, $rs-theme-primary);
+}
 
+// .rs-tab-scroller--animating .rs-tab-scroller__scroll-area {
+//   -webkit-overflow-scrolling: auto;
+// }
+
+// .rs-tab-scroller--animating .rs-tab-scroller__scroll-content {
+//   transition: 250ms transform $rs-animation-standard-curve-timing-function;
+// }
 
 
 </style>
--- packages/tabs/src/rs-tab.vue
+++ packages/tabs/src/rs-tab.vue
@@ -4,30 +4,102 @@
       <span class="rs-tab__text-label" ref="slotContainer">
         <slot></slot>
       </span>
-      <span class="rs-tab-indicator">
-        <span class="rs-tab-indicator__content" aria-hidden="true"/>
-      </span>
     </span>
+    <!-- <span class="rs-tab-indicator">
+      <span class="rs-tab-indicator__content rs-tab-indicator__content--underline" aria-hidden="true"/>
+    </span> -->
     <span class="rs-tab__ripple" />
   </button>
 </template>
 <script>
+import { RSRipple } from '@rsmdc/ripple'
+
 export default {
+  props: {
+    activated: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      el: '',
+      host: ''
+    }
+  },
+  watch: {
+    activated() {
+      this.toggleActivated(this.activated, this.el)
+    },
+    el() {
+      this.host = this.el.parentNode.host
+    },
+    host() {
+      this.toggleActivated(this.activated, this.el, this.host)
+      window.__rsmdc.tab.tabs.push(this.host)
+    }
+  },
+  created() {
+    if(!window.__rsmdc) {
+      window.__rsmdc = {}
+    }
+    if(!window.__rsmdc.tab) {
+      window.__rsmdc.tab = {
+        tabs: []
+      }
+    }
+  },
   mounted() {
     this.$nextTick().then(this.fixSlot.bind(this))
+    new RSRipple(this.$el.querySelector('.rs-tab__ripple'))
     this.el = this.$el
   },
   methods: {
     fixSlot() {
       this.$refs.slotContainer.innerHTML = ''
       this.$refs.slotContainer.append(document.createElement('slot'))
+    },
+    updateTabs() {
+      const tabs = window.__rsmdc.tab.tabs
+      tabs.forEach(tab => {
+        const el = tab.shadowRoot.querySelector('.rs-tab')
+        this.toggleActivated(tab.isEqualNode(this.host), el, this.host)
+      })
+    },
+    toggleActivated(conditions, el, host) {
+      if(conditions) {
+        el.classList.add('-rs-activated')
+      } else {
+        el.classList.remove('-rs-activated')
+      }
     }
   }
 }
 </script>
 <style lang="scss">
+@import "../variables";
 @import "@rsmdc/ripple/mixins";
-@import "@rsmdc/theme/mixins";
+@import "@rsmdc/rtl/mixins";
+@import "@rsmdc/theme/variables";
+@import "@rsmdc/typography/mixins";
+
+@include rs-ripple-common;
+
+:host {
+  position: relative;
+}
+
+:host([activated])::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  transition: 250ms transform $rs-animation-standard-curve-timing-function;
+  height: var(--rs-tab-indicator--height, 2px);
+  width: var(--rs-tab-indicator--width, 100%);
+}
 
 .rs-tab {
   @include rs-typography(button);
@@ -50,6 +122,15 @@ export default {
   flex: var(--rs-tab--flex, 1 0 auto);
 }
 
+// activated tab
+.-rs-activated {
+  .rs-tab__text-label {
+    color: var(--rs-tab-text-label--color, $rs-theme-primary);
+    transition-delay: 100ms;
+    opacity: 1;
+  }
+}
+
 .rs-tab__content {
   position: relative;
   display: flex;
@@ -65,33 +146,42 @@ export default {
   line-height: 1;
   transition: 150ms color linear, 150ms opacity linear;
   z-index: 2;
-
   color: var(--rs-tab-text-label--color, $rs-theme-on-surface);
 }
 
 .rs-tab-indicator {
-  @include rs-tab-indicator-icon-color(secondary);
-  @include rs-tab-indicator-icon-height(34px);
-
   display: flex;
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  pointer-events: none;
   z-index: 1;
   align-self: flex-end;
   width: 100%;
-  transition: 250ms transform $rs-animation-standard-curve-timing-function;
 
-  background-color: var(--rs-tab-indicator-content--background-color, $rs-theme-primary);
-  height: var(--rs-tab-indicator-content--height, 2px);
+  &.rs-tab-indicator--fade > .rs-tab-indicator__content {
+    transition: 150ms opacity linear;
+  }
+
+  &.rs-tab-indicator--active.rs-tab-indicator--fade > .rs-tab-indicator__content {
+    transition-delay: 100ms;
+  }
 }
 
 .rs-tab-indicator__content {
   transform-origin: left;
   opacity: 0;
+
+  &.rs-tab-indicator__content--underline {
+    align-self: flex-end;
+    width: 100%;
+
+    height: var(--rs-tab-indicator-content--height, 2px);
+    background-color: var(--rs-tab-indicator-content--background-color, $rs-theme-primary);
+    border-top-left-radius: var(--rs-tab-indicator-content--border-top-left-radius);
+    border-top-right-radius: var(--rs-tab-indicator-content--border-top-right-radius);
+  }
 }
 
 .rs-tab__ripple {
@@ -105,9 +195,45 @@ export default {
   width: 100%;
   height: 100%;
   overflow: hidden;
+
+  &::before{
+    background-color: var(--rs-ripple_before--background-color, $rs-theme-primary);
+    content: var(--rs-ripple_before--content, '');
+  }
+
+  &::after {
+    background-color: var(--rs-ripple_after--background-color, $rs-theme-primary);
+    content: var(--rs-ripple_after--content, '');
+  }
+
+  &:hover::before {
+    opacity: var(--rs-ripple_hover_before--opacity, rs-states-opacity(primary, hover));
+  }
+
+  &:not(.rs-ripple-upgraded):focus::before { // @mixin rs-states-focus-opacityのfalse
+    transition-duration: var(--rs-ripple_not-upgraded_focus_before--transition-duration, 75ms);
+    opacity: var(--rs-ripple_not-upgraded_focus_before--opacity, rs-states-opacity(primary, focus));
+  }
+
+  &.rs-ripple-upgraded--background-focused::before {
+    transition-duration: var(--rs-upgraded_-background-focused_before--transition-duration, 75ms);
+    opacity: var(--rs-upgraded_-background-focused_before--opacity, rs-states-opacity(primary, focus));
+  }
+
+  &:not(.rs-ripple-upgraded) {
+    &::after {
+      transition: var(--rs-ripple_not-upgraded_after--transition, opacity $rs-ripple-fade-out-duration linear);
+    }
+
+    &:active::after {
+      transition-duration: var(--rs-ripple_not-upgraded_active_after--transition-duration, $rs-ripple-fade-in-duration);
+      opacity: var(--rs-ripple_not-upgraded_active_after--opacity, rs-states-opacity(primary, press));
+    }
+  }
+
+  &.rs-ripple-upgraded {
+    --rs-ripple-fg-opacity: var(--rs-ripple-upgraded--rs-ripple-fg-opacity, #{rs-states-opacity(primary, press)});
+  }
 }
-
-
-
 </style>
 
