2419925ae8ae9b1a16388bced6dbee2fe34fa46c
smspp <smspring0426@gmail.com>
2019-06-05 10:42:21 +0900

    [WIP] change text-field to custom elements

--- packages/textfield/src/rs-text-field.vue
+++ packages/textfield/src/rs-text-field.vue
@@ -3,6 +3,7 @@
     <input type="text" class="rs-text-field__input"
       :value="value" :maxLength="maxLength" :placeholder="placeholder" :autocomplete="autocomplete" :required="isRequired" :disabled="isDisabled">
     <slot></slot>
+    <div class="rs-line-ripple" />
   </div>
 </template>
 <script>
@@ -66,5 +67,57 @@ export default {
 </script>
 
 <style lang="scss">
+
+
+.rs-text-field {
+  @include rs-text-field_;
+
+  &[disabled] {
+    @include rs-text-field-disabled_;
+  }
+
+  &[invalid] {
+    @include rs-text-field-invalid_;
+  }
+}
+
+
+.floating-label {
+  &[disabled] {
+    @include rs-text-field-label-ink-color_($rs-text-field-disabled-label-color);
+  }
+}
+
+
+.rs-notched-outline {
+  @include rs-notched-outline-color($color);
+
+}
+
+.rs-text-field--textarea {
+
+}
+
+.rs-text-field__input {
+  @include rs-text-field-input_;
+}
+
+.rs-text-field-character-counter {
+
+  //.rs-text-field-character-counter + .rs-text-field__input
+  .rs-text-field--textarea & {
+    margin-bottom: 28px; // Leaves space for character counter if it exists.
+    padding-bottom: 0;
+  }
+}
+
+.rs-floating-label {
+  .rs-text-field--textarea & {
+    top: 17px;
+    bottom: auto;
+    width: auto;
+    pointer-events: none;
+  }
+}
 
 </style>
