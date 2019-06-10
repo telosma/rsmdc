774a12aa79839b8034b7037a3d9e941d2bc3e8ee
smspp <smspring0426@gmail.com>
2019-06-10 11:07:33 +0900

    add ripple

--- packages/textfield/src/rs-text-field.vue
+++ packages/textfield/src/rs-text-field.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="rs-text-field -textfield" ref="slotContainer">
+  <div class="rs-text-field -textfield" ref="slotContainer" @click="activateRipple">
     <input type="text" class="rs-text-field__input"
       :value="value" :maxLength="maxLength" :placeholder="placeholder" :autocomplete="autocomplete" :required="isRequired" :disabled="isDisabled">
     <div class="rs-line-ripple" />
   </div>
 </template>
 <script>
+import { RSRipple } from '@rsmdc/ripple'
+
+import { RSLineRipple } from '../../line-ripple/index'
+
 export default {
   props: {
     maxlength: {
@@ -31,6 +35,9 @@ export default {
   },
   data() {
     return {
+      el: '',
+      host: '',
+      lineRipple: '',
       isRequired: false,
       isDisabled: false,
       textLength: 0
@@ -42,9 +49,15 @@ export default {
     },
     required() {
 
+    },
+    el() {
+      this.host = this.el.parentNode.host
     }
   },
   mounted() {
+    const ripple = new RSRipple(this.$el)
+    this.lineRipple = new RSLineRipple(this.$el.querySelector('.rs-line-ripple'))
+    this.el = this.$el
     // this.$nextTick().then(this.fixSlot.bind(this))
   },
   methods: {
@@ -56,6 +69,18 @@ export default {
       const style = window.getComputedStyle(el)
       const value = String(style.getPropertyValue(prop)).trim()
       return value
+    },
+    activateRipple() {
+      this.lineRipple.activate()
+      this.$el.classList.add('rs-ripple-upgraded--background-focused')
+      this.el.querySelector('.rs-text-field__input').focus()
+      this.host.addEventListener('blur', () => {
+        this.deactivateRipple()
+      })
+    },
+    deactivateRipple() {
+      this.lineRipple.deactivate()
+      this.$el.classList.remove('rs-ripple-upgraded--background-focused')
     }
   }
 }
