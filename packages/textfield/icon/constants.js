277556d54fba9b3919ba43a66dfa00ce874fb0dc
smspp <smspring0426@gmail.com>
2019-06-12 13:40:54 +0900

    remove unnecessary file

--- packages/textfield/icon/constants.js
+++
@@ -1,5 +0,0 @@
-var strings = {
-    ICON_EVENT: 'rsTextField:icon',
-    ICON_ROLE: 'button',
-};
-export { strings };
--- packages/textfield/index.js
+++
@@ -1,5 +0,0 @@
-export * from './component';
-export * from './foundation';
-export * from './character-counter/index';
-export * from './helper-text/index';
-export * from './icon/index';
