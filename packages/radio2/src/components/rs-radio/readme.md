95fc459521397a201b3db53a9e98397c4f4b80a3
kokami <mooooonaka@gmail.com>
2019-08-02 11:03:52 +0900

    feat: Scss rewrite

--- packages/radio2/src/components/rs-radio/readme.md
+++ packages/radio2/src/components/rs-radio/readme.md
@@ -9,6 +9,7 @@
 
 | Property | Attribute | Description | Type     | Default     |
 | -------- | --------- | ----------- | -------- | ----------- |
+| `id`     | `id`      |             | `string` | `undefined` |
 | `label`  | `label`   |             | `string` | `undefined` |
 
 
--- packages/radio2/src/components/rs-radio/rs-radio.tsx
+++ packages/radio2/src/components/rs-radio/rs-radio.tsx
@@ -1,28 +1,31 @@
-import { Component, Prop, Host, h } from '@stencil/core';
+import { Component, Prop, Host, h } from "@stencil/core";
 
 @Component({
-  tag: 'rs-radio',
+  tag: "rs-radio",
   // styleUrl: 'my-component.css',
   shadow: true
 })
 export class Radio {
+  @Prop() id: string;
 
-  @Prop() id: string
-
-  @Prop() label: string
+  @Prop() label: string;
 
   render() {
-     return <Host>
-              <div class="rs-radio">
-                <div class="rs-radio__container">
-                  <input class="rs-radio__native-control" type="radio" />
-                  <div class="rs-radio__background">
-                    <div class="rs-radio__outer-circle"></div>
-                    <div class="rs-radio__inner-circle"></div>
-                  </div>
-                </div>
-                <label class="rs-radio__label" htmlFor={this.id}>{this.label}</label>
-              </div>
-            </Host>
+    return (
+      <Host>
+        <div class="rs-radio">
+          <div class="container">
+            <input class="nativecontrol" type="radio" />
+            <div class="background">
+              <div class="outercircle" />
+              <div class="innercircle" />
+            </div>
+          </div>
+          <label class="label" htmlFor={this.id}>
+            {this.label}
+          </label>
+        </div>
+      </Host>
+    );
   }
 }
