c2f277edb2ad042934dab229914306d650cebd4c
smspp <smspring0426@gmail.com>
2019-08-14 01:11:10 +0900

    feat: [WIP] Add app-layout

---
+++ packages/app-layout2/src/components/rs-app-layout/rs-app-layout.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'rs-app-layout',
+  styleUrl: '../../dist/result.css',
+  shadow: true
+})
+export class MyComponent {
+
+  render() {
+    return <div></div>;
+  }
+}
