35fbdd53391c6d3af4aa86ae578bf2791ef914ff
smspp <smspring0426@gmail.com>
2019-09-03 15:50:15 +0900

    feat: Add drawer component

---
+++ packages/drawer2/src/components/rs-drawer/rs-drawer-header.tsx
@@ -0,0 +1,16 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'rs-drawer-header',
+  styleUrl: '../../dist/result.css',
+  shadow: true
+})
+export class DrawerHeader {
+  render() {
+    return  <Host>
+              <header class="rs-drawer-header">
+                <slot />
+              </header>
+            </Host>
+  }
+}
